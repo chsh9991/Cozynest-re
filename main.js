4b6150ea65e0db35178906270ebee51940932f18
chsh9991 <128586724+chsh9991@users.noreply.github.com>
2023-10-24 04:18:44 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -23,22 +23,16 @@ $(function(){
 
 // 메뉴 버튼 안 서브메뉴 기능
 $(function(){
-    let tabmenu = $('.over .menu li .menu_text')
-        tablist = $('.menu_sub')
-
-        tabmenu.click(function(e){
-            e.preventDefault();
-
-            tabmenu.removeClass('active');
-            $(this).addClass('active')
-            
-            tablist.hide();
-
-            let $taget = $(this).attr('href');
-
-            $($taget).css({display:'flex'});
-        })
+    const tabMenu = $('.over .menu li a');
+    const tabSub = $('.over .menu_sub')
+    tabMenu.click(function(){
+        $(this).next().slideToggle().siblings('.menu_sub').slideUp();
+    })
+    $('.menu_sub').hide();
 });
+
+
+
 
 // 컨테이너 1 슬라이드
 $(function(){
